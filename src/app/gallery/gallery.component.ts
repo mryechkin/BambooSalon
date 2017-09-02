61769a2f88b4fc6c88c236ff7eda768bdf6b0c2c
Mykhaylo Ryechkin <mykhaylo.ryechkin@gmail.com>
2017-09-02 01:00:00 -0400

    Adding Gallery and The Bald Barber

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from "../data.service";
 
 @Component({
   selector: 'app-gallery',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
-  constructor() { }
+  pageData = <any>{};
+
+  constructor(private dataService: DataService) {
+    this.dataService.getDataFromUrl('assets/data/gallery.json').subscribe(
+      (res) => {
+        this.pageData = res;
+      },
+      (error) => console.log('Error fetching page data: ' + error)
+    )
+  }
 
   ngOnInit() {
   }
--- src/app/thebaldbarber/thebaldbarber.component.ts
+++ src/app/thebaldbarber/thebaldbarber.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from "../data.service";
 
 @Component({
   selector: 'app-thebaldbarber',
@@ -7,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThebaldbarberComponent implements OnInit {
 
-  constructor() { }
+  pageData = <any>{};
+  imagesLeft = [
+    'assets/images/thebaldbarber/thebaldbarber1.jpg',
+    'assets/images/thebaldbarber/thebaldbarber2.jpg',
+    'assets/images/thebaldbarber/thebaldbarber3.jpg',
+    'assets/images/thebaldbarber/thebaldbarber5.jpg'
+  ];
+  imagesRight = [
+    'assets/images/thebaldbarber/thebaldbarber6.jpg',
+    'assets/images/thebaldbarber/thebaldbarber7.jpg',
+    'assets/images/thebaldbarber/thebaldbarber8.jpg',
+    'assets/images/thebaldbarber/thebaldbarber9.jpg'
+  ];
+
+  constructor(private dataService: DataService) {
+    this.dataService.getDataFromUrl('assets/data/thebaldbarber.json').subscribe(
+      (res) => {
+        this.pageData = res;
+      },
+      (error) => console.log('Error fetching page data: ' + error)
+    )
+  }
 
   ngOnInit() {
   }
